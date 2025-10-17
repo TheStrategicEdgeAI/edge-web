a7f643a51a99e9f1dced9e5bd0ebe15f54929649
TheStrategicEdgeAI <thestrategicedgeai@gmail.com>
2025-10-17 10:01:27 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,6 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { SessionProvider } from 'next-auth/react';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <SessionProvider session={(pageProps as any).session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+export default function EdgeApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
