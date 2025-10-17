d8d8c0c9e6be04526d442146c7f9e32c9c4f344c
TheStrategicEdgeAI <thestrategicedgeai@gmail.com>
2025-10-17 13:59:36 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+import { AuthProvider } from '../context/AuthContext';
+import NavBar from '../components/NavBar';
 
 export default function EdgeApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      {/* Global navigation bar displayed on every page */}
+      <NavBar />
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
