520bf12639d98a2c03b3605ec2abc8f6322a7052
TheStrategicEdgeAI <thestrategicedgeai@gmail.com>
2025-10-17 14:16:04 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import { AuthProvider } from '../context/AuthContext';
 import NavBar from '../components/NavBar';
+import EdgeChat from '../components/EdgeChat';
 
 export default function EdgeApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +10,8 @@ export default function EdgeApp({ Component, pageProps }: AppProps) {
       {/* Global navigation bar displayed on every page */}
       <NavBar />
       <Component {...pageProps} />
+      {/* Global chat bubble for quick help */}
+      <EdgeChat />
     </AuthProvider>
   );
 }
