288b3d7bdef0a13db6f89b713199dfdc6bdda8a9
TheStrategicEdgeAI <thestrategicedgeai@gmail.com>
2025-10-16 22:23:34 -0700

    Create signup.tsx

---
+++ pages/signup.tsx
@@ -0,0 +1,7 @@
+export default function Signup() {
+  return (
+    <div className="min-h-screen p-8 flex items-center justify-center text-center">
+      Signup page (under construction)
+    </div>
+  );
+}
